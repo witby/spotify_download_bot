0bdf9e11baa0297a2c4a90c611123a6a207865cf
witby <47345574+witby@users.noreply.github.com>
2021-05-01 11:28:12 +0800

    Update bot.py

--- bot.py
+++ bot.py
@@ -16,7 +16,7 @@ def setup_logging():
 
 
 def start(update: Update, context: CallbackContext):
-    update.effective_message.reply_text("شایگاننننننننن")
+    update.effective_message.reply_text("Hello, this is a bot for enjoying spotify music!")
     context.bot.send_message(
         chat_id=update.effective_chat.id, text="/spotify [url]"
     )
